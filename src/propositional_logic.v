eea7b34f8c7c87c027079f111b2bdd3ddbc26bee
Raghuram Sundararajan <raghurams13@protonmail.com>
2022-06-05 00:17:09 +0530

    Refactor: change col width sometimes and use `Context` + implicit argument detection.

--- src/propositional_logic.v
+++ src/propositional_logic.v
@@ -148,12 +148,14 @@ Fixpoint proof_trans p (proof : Γ' |- p) : Γ |- p := match proof with
 | rule_1 p q             => rule_1 p q
 | rule_2 p q r           => rule_2 p q r
 | by_contradiction p     => by_contradiction p
-| modus_ponens proof_hyp proof_imp => modus_ponens (proof_trans proof_hyp) (proof_trans proof_imp)
+| modus_ponens proof_hyp proof_imp => modus_ponens (proof_trans proof_hyp)
+                                                   (proof_trans proof_imp)
 end.
 
-(* Remark: it is not possible to derive trans' without switching to `Provable` instead of
-   `Proof` or assuming some decidability, because otherwise, given `p ∈ Γ ∪ Γ'`, we can't
-   tell whether p is in Γ or Γ', and that affects the proof of `Γ |- p` constructed. *)
+(* Remark: it is not possible to derive trans' without switching to `Provable`
+   instead of `Proof` or assuming some decidability, because otherwise, given
+   `p ∈ Γ ∪ Γ'`, we can't tell whether p is in Γ or Γ', and that affects the
+   proof of `Γ |- p` constructed. *)
 Fixpoint proof_trans' {dec : forall p, ({p ∈ Γ}+{p ∉ Γ}) + ({p ∈ Γ'}+{p ∉ Γ'})}
     p (proof : Γ ∪ Γ' |- p) : Γ |- p := match proof in _ |- p return Γ |- p with
 | by_assumption h_in_union   => match sumbool_of_dec_either h_in_union (dec _) with
@@ -169,7 +171,8 @@ end.
 
 Definition provable_trans' (h : forall [p], p ∈ Γ' -> [Γ |- p])
     p (proof : [Γ ∪ Γ' |- p]) : [Γ |- p] := let (proof) := proof in
-(* Fixpoint doesn't work because we can't do structural induction on Provable, only on Proof. *)
+(* Fixpoint doesn't work because we can't do structural induction on Provable,
+   only on Proof. *)
 (fix proof_trans' [p] (proof : Γ ∪ Γ' |- p) : [Γ |- p] :=
 match proof in _ |- p return [Γ |- p] with
 | by_assumption (or_introl h_in)  => by_assumption h_in
@@ -185,7 +188,7 @@ end
 
 End Transitivity.
 
-Definition proof_of_proof_subset (Γ Γ' : unary_predicate Proposition) (h : Γ ⊆ Γ')
+Definition proof_of_proof_subset Γ Γ' (h : Γ ⊆ Γ')
     : forall p, Γ |- p -> Γ' |- p :=
 proof_trans (fun _ h_in => by_assumption (h _ h_in)).
 
@@ -197,9 +200,9 @@ Check fun Γ p (proof : [Γ |- p]) => (provable_trans' (eq_ind p _ proof)
 End RelationBetweenDifferentAssumptions.
 
 Section SomeLemmas.
-Variables (Γ : unary_predicate Proposition) (p q r : Proposition).
+Context {Γ : unary_predicate Proposition}.
 
-Definition id : Γ |- (p '-> p) :=
+Definition id p : Γ |- (p '-> p) :=
 let step_1 : Γ |- p '-> (p '-> p) '-> p         := rule_1 p (p '-> p) in
 let step_2 : Γ |- p '-> p '-> p                 := rule_1 p p in
 let step_3 : Γ |- (p '-> (p '-> p) '-> p) '-> (p '-> p '-> p) '-> (p '-> p)
@@ -207,18 +210,18 @@ let step_3 : Γ |- (p '-> (p '-> p) '-> p) '-> (p '-> p '-> p) '-> (p '-> p)
 let step_4 : Γ |- (p '-> p '-> p) '-> (p '-> p) := modus_ponens step_1 step_3 in
 modus_ponens step_2 step_4.
 
-Definition add_under_imp (proof : Γ |- q) : Γ |- (p '-> q) :=
+Definition add_under_imp p q (proof : Γ |- q) : Γ |- (p '-> q) :=
 let step_1 : Γ |- q '-> p '-> q := rule_1 q p in
 modus_ponens proof step_1.
 
-Definition modus_ponens_under_imp
-    : let P' q := Γ |- (p '-> q) in   P' q -> P' (q '-> r) -> P' r :=
+Definition modus_ponens_under_imp p hyp concl
+    : let P' q := Γ |- (p '-> q) in   P' hyp -> P' (hyp '-> concl) -> P' concl :=
 fun proof_hyp proof_imp =>
-let step_1 : Γ |- (p '-> q '-> r) '-> (p '-> q) '-> (p '-> r) := rule_2 p q r in
+let step_1 : Γ |- (p '-> hyp '-> concl) '-> (p '-> hyp) '-> (p '-> concl) := rule_2 p hyp concl in
 let step_2 := modus_ponens proof_imp step_1 in
 modus_ponens proof_hyp step_2.
+
 End SomeLemmas.
-Arguments id {Γ} {p}.
 
 Section DeductionTheorem.
 Variables (Γ : unary_predicate Proposition) (p q : Proposition).
