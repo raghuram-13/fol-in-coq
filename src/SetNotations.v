1c2f8ff02fb10ea4bee7cd0deabf3746e42a700d
Raghuram Sundararajan <raghurams13@protonmail.com>
2022-06-11 15:12:51 +0530

    Format to 80-character width in src/SetNotations.v and src/Lattices.v.
    Also add a few organisational comments, etc.

--- src/SetNotations.v
+++ src/SetNotations.v
@@ -1,27 +1,37 @@
 Require Import Coq.Classes.RelationClasses.
 
+(* Subsets *)
+
 Export Coq.Classes.RelationClasses (unary_predicate).
 
 Notation "x ∈ S" := ((S : unary_predicate _) x) (at level 70).
 Notation "x ∉ S" := (~(S : unary_predicate _) x) (at level 70).
 
-(*
-Local Notation "{ x : U ∣ P }" := (fun x : U => P) (x binder, x at level 99) : subset_scope.
-Local Notation "{ x | P }"     := (fun x => P)     (x binder, x at level 99) : subset_scope.
-*)
-
 Declare Scope subset_scope. Bind Scope subset_scope with unary_predicate.
 Delimit Scope subset_scope with subset_scope.
 
+(*
+Local Notation "{ x : U ∣ P }" := (fun x : U => P)
+    (x binder, x at level 99) : subset_scope.
+Local Notation "{ x | P }"     := (fun x => P)
+    (x binder, x at level 99) : subset_scope.
+*)
+
 Notation "∅"     := (false_predicate : unary_predicate _) : subset_scope.
-Notation "A ∪ B" := (predicate_union _ (A : unary_predicate _) (B : unary_predicate _))
-                      (at level 65, right associativity)  : subset_scope.
-Notation "A ∩ B" := (predicate_intersection _ (A : unary_predicate _) (B : unary_predicate _))
-                      (at level 65, right associativity)  : subset_scope.
-Notation "A ⊆ B" := (predicate_implication (A : unary_predicate _) (B : unary_predicate _))
-                      (at level 70)                       : subset_scope.
+Notation "A ∪ B" :=
+  (predicate_union _        (A : unary_predicate _) (B : unary_predicate _))
+    (at level 65, right associativity)                    : subset_scope.
+Notation "A ∩ B" :=
+  (predicate_intersection _ (A : unary_predicate _) (B : unary_predicate _))
+    (at level 65, right associativity)                    : subset_scope.
+Notation "A ⊆ B" :=
+  (predicate_implication    (A : unary_predicate _) (B : unary_predicate _))
+    (at level 70)                                         : subset_scope.
 
 Open Scope subset_scope.
+
+
+(* Type-valued predicates *)
 
 Module MyExtensions. Module Classes. Module RelationClasses.
 Section ForVariables.
@@ -47,6 +57,8 @@ End ForVariables.
 End RelationClasses. End Classes. End MyExtensions.
 Import MyExtensions.Classes.RelationClasses.
 
-(* Some other notations. *)
-Notation "A ⊔ B" := (pointwise_extension sum (Tcons _ Tnil) A B) (at level 67, left associativity) : function_scope.
-Notation "A ⊑ B" := (pointwise_lifting' Coq.Program.Basics.arrow (l := Tcons _ Tnil) A B) (at level 70) : function_scope.
+Notation "A ⊔ B" := (pointwise_extension sum (Tcons _ Tnil) A B)
+    (at level 67, left associativity) : function_scope.
+Notation "A ⊑ B" :=
+  (pointwise_lifting' Coq.Program.Basics.arrow (l := Tcons _ Tnil) A B)
+    (at level 70) : function_scope.
