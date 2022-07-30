fab3bccf501d3b8d06e9ad366289c8b7670c34a8
Raghuram Sundararajan <raghurams13@protonmail.com>
2022-07-30 19:05:08 +0530

    Give up on many-sorted FOL (at least, for now) and define substitution.
    Thus, change
    + heterolist into Array
    + occurrence into BNat
    + `vararg` functions to take a nat and a type rather than a list
      (indexing) types
    + Additionally, remove the extra `types` parameter of the signature and
      all the `type` arguments
    + etc.
    
    Additionally, complete definition of substitution by an array of
    values for the full context, rather than for a single variable. This
    eliminates the huge struggle of trying to define substitution of a
    single variable by a single term, involving trying to prove decidable
    equality on `BNat`, etc.
    
    This idea came from a few resources on first-order logic.

--- src/first_order_logic.v
+++ src/first_order_logic.v
@@ -1,24 +1,35 @@
 Set Implicit Arguments. Unset Strict Implicit. Set Maximal Implicit Insertion.
 
 From Coq Require Lists.List.
-Require Import Util. Import (notations) Util.HeterolistNotations.
+Require Import Util. Import (notations) Util.Array.Notation.
 
 Section Syntax.
 
 (* The signature of a particular FOL. *)
 
-(* We are building many-sorted FOL. This hopefully shouldn't add much
-   complexity to the proofs of the fundamental theorems. *)
-Variable types : Type.
-Variable functions : list types -> types -> Type.
-Variable predicates : list types -> Type.
+(* We are building single-sorted FOL. *)
+Variable functions : nat -> Type.
+Variable predicates : nat -> Type.
 
-Inductive Term (type_context : list types) | (type : types) :=
-| var (_ : occ type type_context)
-| app {arg_types} (function : functions arg_types type)
-                  (args : heterolist Term arg_types).
+Inductive Term (context : nat) :=
+| var (_ : BNat context)
+| app {arity} (function : functions arity) (args : Array (Term context) arity).
 
-Definition ClosedTerm := Term nil.
+Definition ClosedTerm := Term 0.
+
+(* This comes with no motives because I've abandoned heterogenous lists and arrays. *)
+Definition Term_rect' {context} {P : Type}
+  (var : BNat context -> P)
+  (app : forall {arity}, functions arity -> Array (Term context) arity -> Array P arity -> P)
+  : Term context -> P :=
+fix Term_rect' (term : Term context) := match term with
+| var a => var a
+| app f args => app f args
+                  ((fix ArrayTerm_rect' {n} args := match args with
+                   | []          => []
+                   | arg :: rest => Term_rect' arg :: ArrayTerm_rect' rest
+                   end) _ args)
+end.
 
 (* Note: this has unnecessary repetition in that it repeats the
     operations of propositional logic and this might force us to redo
@@ -26,39 +37,75 @@ Definition ClosedTerm := Term nil.
    Possible fix: rewrite propositional_logic to not assume free
     propositional language on a set of variables, and have the free
     case as an instance? *)
-Inductive Formula | (type_context : list types) :=
-| predApp {arg_types} (predicate : predicates arg_types)
-                      (args : heterolist (Term type_context) arg_types)
+Inductive Formula | (context : nat) :=
+| predApp {arity} (predicate : predicates arity)
+                  (args : Array (Term context) arity)
 | false
-| impl : Formula type_context -> Formula type_context -> Formula type_context
-| univ {type} : Formula (type :: type_context) -> Formula type_context.
-#[global] Arguments false {type_context}.
+| impl : Formula context -> Formula context -> Formula context
+| univ : Formula (S context) -> Formula context.
+#[global] Arguments false {context}.
 
-Definition Sentence := Formula nil.
+Definition Sentence := Formula 0.
 
 (* Derived operations. *)
 (* Mainly defined to use for the notations. *)
-Definition neg {type_context} (φ : Formula type_context)
-  : Formula type_context :=
+Definition neg {context} (φ : Formula context)
+  : Formula context :=
 impl φ false.
 
-Definition exist {type_context type} (φ : Formula (type::type_context))
-  : Formula type_context :=
+Definition exist {context} (φ : Formula (S context))
+  : Formula context :=
 neg (univ (neg φ)).
 
 Section Substitution.
-Context {type_context : list types} {type : types}
-        (variable : occ type type_context) (value : Term type_context type).
 
-Fixpoint term_subst [type'] (term : Term type_context type')
-  : Term (remove_occ variable) type' := match term with
-| var o      => _
-| app f args => app f (heteromap term_subst args)
+Fixpoint addContext extraContext {context} (term : Term context) {struct term}
+  : Term (extraContext + context) := match term with
+| var o      => var (BNat.toSum_a extraContext o)
+| app f args => app f (* Array.map (addContext extraContext) args *)
+                      ((fix Array_map {n} (args : Array (Term context) n) := match args with
+                       | []          => []
+                       | arg :: rest => addContext extraContext arg :: Array_map rest
+                       end) _ args)
 end.
 
+Section TermSubst.
+Context {context context' : nat} (values : Array (Term context) context').
+
+Definition term_subst : Term context' -> Term context :=
+(* match term with
+| var o      => Array.ref o values
+| app f args => app f (Array.map term_subst args)
+end. *)
+Term_rect' (fun a : BNat context' => Array.ref a values)
+           (fun _ f _ args' => app f args').
+End TermSubst.
+
+(* This now works pretty much exactly like
+   _Completeness theorems for first-order logic, analysed in constructive type theory_
+   and other sources.
+   Recursion except through quantifiers (or anything that changes the
+    context) is straightforward, and for moving under a quantifier we
+    increment the de Bruijn indexes (achieved by `Array.map (addContext 1)`) and
+    add an identity substitution at the front (achieved by
+    `var BNat_zero ::`). *)
+Fixpoint formula_subst {context context' : nat} (values : Array (Term context) context')
+                       (formula : Formula context') : Formula context :=
+match formula with
+| predApp r args => predApp r (Array.map (term_subst values) args)
+| false          => false
+| impl p q       => impl (formula_subst values p) (formula_subst values q)
+| univ formula   => univ (formula_subst (context := S context)
+                                          (var BNat_zero :: Array.map (addContext 1)
+                                                                      values)
+                                        formula)
+end.
+
+End Substitution.
+
 End Syntax.
-#[global] Arguments var {types functions type_context type}.
-#[global] Arguments false {types functions predicates type_context}.
+#[global] Arguments var {functions context}.
+#[global] Arguments false {functions predicates context}.
 
 Module FOLFormulaNotations.
   Declare Scope first_order_formula.
@@ -76,82 +123,67 @@ Module FOLFormulaNotations.
 
   (* Example: given a predicate symbol φ with one argument of type t,
      the formula `∃ x, ¬(φ x)`. *)
-  (* Check fun t φ => ∃.¬(predApp φ [var (occ_head t nil)]). *)
+  (* Check fun t φ => ∃.¬(predApp φ [var (* (occ_head t nil) *) BNat_zero]). *)
 End FOLFormulaNotations.
 
 
 Section Semantics.
-Variables (types : Type) (functions : list types -> types -> Type)
-                         (predicates : list types -> Type).
+Variables (functions : nat -> Type) (predicates : nat -> Type).
 
 Structure Model := {
-  modelType : types -> Type;
-  modelFun {arg_types : list types} {res_type : types}
-    : functions arg_types res_type
-      -> vararg_function modelType arg_types (modelType res_type);
-  modelPred {arg_types : list types}
-    : predicates arg_types -> vararg_predicate modelType arg_types
+  modelType : Type;
+  modelFun {arity : nat}
+    : functions arity -> vararg_function arity modelType modelType;
+  modelPred {arity : nat}
+    : predicates arity -> vararg_predicate arity modelType
 }.
 
 Section Interpretation.
 Set Strict Implicit. Context (m : Model).
 
 Section value.
-Context [type_context] (context : heterolist m.(modelType) type_context).
-Fixpoint value [type] (term : Term functions type_context type)
-  : m.(modelType) type := match term with
-| var occ    => ref_by_occ occ context
+Context [context] (values : Array m.(modelType) context).
+Fixpoint value (term : Term functions context)
+  : m.(modelType) := match term with
+| var occ    => Array.ref occ values
 | app f args =>
   vararg_apply (m.(modelFun) f)
-    (* This runs into the problem that `value` is passed
-       to `heteromap` unapplied to two arguments, rather
-       than just the decreasing argument `term`.
-
-       Ideally we want the Fixpoint declaration to be
-       interpreted as
-       "term decreases and type changes arbitrarily", but
-       it seems it is perhaps interpreted as
-       "term decreases and type remains the same"
-       instead. `type` is literally just an index for the
-       type of term; this shouldn't be a problem at all. *)
-    (* (heteromap value args) *)
-    ((fix map_value [is] (l : heterolist (Term functions type_context) is)
-      : heterolist m.(modelType) is := match l with
+    (* (Array.map value args) *)
+    ((fix map_value [is] (l : Array (Term functions context) is)
+      : Array m.(modelType) is := match l with
     | []           => []
     | term :: rest => value term :: map_value rest
     end) _ args)
 end.
 End value.
 
-(* Can't use type_context, context section variables because context has to
+(* Can't use context, values section variables because values has to
    vary in the recursive calls. *)
 Fixpoint interpret
-    [type_context] (context : heterolist m.(modelType) type_context)
-  (φ : Formula functions predicates type_context) : Prop := match φ with
+    [context] (values : Array m.(modelType) context)
+  (formula : Formula functions predicates context) : Prop := match formula with
 | predApp r args       => vararg_apply (m.(modelPred) r)
-                            (heteromap (value context) args)
+                            (Array.map (value values) args)
 | false                => False
-| impl p q             => interpret context p -> interpret context q
-| @univ _ _ _ _ type φ =>
-  forall x : m.(modelType) type, interpret (x :: context) φ
+| impl p q             => interpret values p -> interpret values q
+| univ formula => forall x : m.(modelType), interpret (x :: values) formula
 end.
 
 End Interpretation.
 End Semantics.
 
 Section Proofs.
-Context {types : Type} {functions : list types -> types -> Type}
-                       {predicates : list types -> Type}.
+Context {types : Type} {functions : nat -> Type} {predicates : nat -> Type}.
 
 Section defs.
 
 #[local] Notation "'Assumptions'" :=
-(forall type_context, Formula functions predicates type_context -> Type)
+(forall context, Formula functions predicates context -> Type)
     (only parsing).
 
 Implicit Types assumptions Γ : Assumptions.
 
-Inductive Proof .
+Inductive Proof.
 
 End defs.
 
@@ -163,25 +195,26 @@ Section Example.
 
 (* We could let these be automatically inferred (as Prop), but we might
    as well specify Set. *)
-Inductive types : Set := | Nat. Check types.
-Inductive functions : list types -> types -> Set :=
-| zero : functions nil  Nat
-| succ : functions (Nat::nil) Nat.
-Inductive relations : list types -> Set :=
-| int_eq : relations (Nat :: Nat :: nil).
+(* Inductive types : Set := | Nat. Check types. *)
+Inductive functions : (* list types -> types *) nat -> Set :=
+| zero : functions (* nil  Nat *) 0
+| succ : functions (* (Nat::nil) Nat *) 1.
+Inductive relations : (* list types *) nat -> Set :=
+| int_eq : relations (* (Nat :: Nat :: nil) *) 2.
 
-Let mysentence := univ (type := Nat)
-                    (impl (predApp int_eq [var (occ_head _ _); app zero []])
-                      (impl (predApp int_eq [var (occ_head _ _); app succ [app zero []]])
+Let mysentence := univ (* (type := Nat) *)
+                    (impl (predApp int_eq [var (* (occ_head _ _) *) BNat_zero; app zero []])
+                      (impl (predApp int_eq [var (* (occ_head _ _) *) BNat_zero; app succ [app zero []]])
                         false)) : Sentence functions relations.
 
 Definition standard_model : Model functions relations := {|
-  modelType x    := nat (* match x with | Nat => nat end *);
-  modelFun _ _ f := match f in functions l _ return vararg_function _ l nat with
+  modelType (* x *)    := nat (* match x with | Nat => nat end *);
+  modelFun _ (* _ *) f := match f in functions l (* _ *)
+                            return vararg_function (* _ *) l nat nat with
     | zero => 0
     | succ => S
     end;
-  modelPred _ r := match r in relations l return vararg_function _ l Prop with
+  modelPred _ r := match r in relations l return vararg_predicate l nat with
     | int_eq => @eq nat
     end
 |}.
